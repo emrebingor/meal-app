a67c9543ab75e3ccf2e46100ed4940f9df78ce8d
emrebingor <emrebingor5@gmail.com>
2022-07-13 13:24:48 +0300

    Categories Screen addition

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app_flutter/dummy_data.dart';
+import 'package:meal_app_flutter/category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-      children: <Widget>[
-
-      ],
+      children: Dummy_Categories.map(
+        (catData) => CategoryItem(
+          catData.title,
+          catData.color,
+        ),
+      ).toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 1.5,
@@ -15,4 +20,3 @@ class CategoriesScreen extends StatelessWidget {
     );
   }
 }
-
