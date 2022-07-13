4818c5805cab2fdbd755534522897c1557004374
emrebingor <emrebingor5@gmail.com>
2022-07-13 17:04:42 +0300

    Navigator addition to category item

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app_flutter/category_meal_screen.dart';
+
 
 class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
 
-  CategoryItem(this.title,this.color);
+  CategoryItem(this.title, this.color);
+
+  void changeCategory(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => CategoryMealScreen(),),);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return InkWell(
+        onTap: () {
+          changeCategory(context);
+        },
+        splashColor: Theme.of(context).primaryColor,
+        borderRadius: BorderRadius.circular(15.0),
+        child: Container(
       padding: EdgeInsets.all(15.0),
-      child: Text(title, style: TextStyle(fontSize: 24.0, fontFamily: 'RobotCondensed'),),
+      child: Text(
+        title,
+        style: TextStyle(
+            fontSize: 20.0,
+            fontFamily: 'RobotCondensed',
+            fontWeight: FontWeight.bold),
+      ),
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [
@@ -20,8 +38,8 @@ class CategoryItem extends StatelessWidget {
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
         ),
-        borderRadius: BorderRadius.circular(15),
+        borderRadius: BorderRadius.circular(15.0),
       ),
-    );
+    ),);
   }
 }
