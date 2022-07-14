72a3ac2425aa810284ae10fe613bc4e6e9e5ec84
emrebingor <emrebingor5@gmail.com>
2022-07-14 13:57:54 +0300

    Displaying Images

--- lib/category_meal_screen.dart
+++ lib/category_meal_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meal_app_flutter/dummy_data.dart';
+import 'package:meal_app_flutter/meal_item.dart';
 
 class CategoryMealScreen extends StatelessWidget {
   static const String id = 'categoryMeal_screen';
@@ -10,15 +11,26 @@ class CategoryMealScreen extends StatelessWidget {
         ModalRoute.of(context)?.settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
-    final categoryMeal = Dummy_Meals.where((meal) {return meal.categories.contains(categoryId);}).toList();
+    final categoryMeal = Dummy_Meals.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle.toString()),
       ),
-      body: ListView.builder(itemBuilder: (context, index){
-        return Text(categoryMeal[index].title);
-      }, itemCount: categoryMeal.length,),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return MealItem(
+            title: categoryMeal[index].title,
+            imageUrl: categoryMeal[index].imageUrl,
+            duration: categoryMeal[index].duration,
+            affordability: categoryMeal[index].affordability,
+            complexity: categoryMeal[index].complexity,
+          );
+        },
+        itemCount: categoryMeal.length,
+      ),
     );
   }
 }
